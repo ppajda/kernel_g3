80735222cb84c31ab70a56249c3847ed38d5294d
ppajda <pajdek@gmail.com>
2017-08-09 13:55:52 +0200

    Maple fixes

--- block/maple-iosched.c
+++ block/maple-iosched.c
@@ -17,20 +17,20 @@
 #include <linux/module.h>
 #include <linux/init.h>
 #include <linux/slab.h>
-#include <linux/display_state.h>
+#include <linux/state_notifier.h>
 
-#define MAPLE_IOSCHED_PATCHLEVEL	(8)
+#define MAPLE_IOSCHED_PATCHLEVEL	(7)
 
 enum { ASYNC, SYNC };
 
 /* Tunables */
-static const int sync_read_expire = 350;	/* max time before a read sync is submitted. */
-static const int sync_write_expire = 550;	/* max time before a write sync is submitted. */
-static const int async_read_expire = 250;	/* ditto for read async, these limits are SOFT! */
-static const int async_write_expire = 450;	/* ditto for write async, these limits are SOFT! */
+static const int sync_read_expire = 100;	/* max time before a read sync is submitted. */
+static const int sync_write_expire = 350;	/* max time before a write sync is submitted. */
+static const int async_read_expire = 200;	/* ditto for read async, these limits are SOFT! */
+static const int async_write_expire = 500;	/* ditto for write async, these limits are SOFT! */
 static const int fifo_batch = 16;		/* # of sequential requests treated as one by the above parameters. */
-static const int writes_starved = 4;		/* max times reads can starve a write */
-static const int sleep_latency_multiple = 10;	/* multple for expire time when device is asleep */
+static const int writes_starved = 3;		/* max times reads can starve a write */
+static const int sleep_latency_multiple = 5; /* multple for expire time when device is asleep */
 
 /* Elevator data */
 struct maple_data {
@@ -62,9 +62,9 @@ maple_merged_requests(struct request_queue *q, struct request *rq,
 	 * and move into next position (next will be deleted) in fifo.
 	 */
 	if (!list_empty(&rq->queuelist) && !list_empty(&next->queuelist)) {
-		if (time_before(next->fifo_time, rq->fifo_time)) {
+		if (time_before(rq_fifo_time(next), rq_fifo_time(rq))) {
 			list_move(&rq->queuelist, &next->queuelist);
-			rq->fifo_time = next->fifo_time;
+			rq_set_fifo_time(rq, rq_fifo_time(next));
 		}
 	}
 
@@ -78,7 +78,7 @@ maple_add_request(struct request_queue *q, struct request *rq)
 	struct maple_data *mdata = maple_get_data(q);
 	const int sync = rq_is_sync(rq);
 	const int dir = rq_data_dir(rq);
-	const bool display_on = is_display_on();
+	const bool display_on = state_suspended;
 
 	/*
 	 * Add request to the proper fifo list and set its
@@ -88,10 +88,10 @@ maple_add_request(struct request_queue *q, struct request *rq)
    	/* inrease expiration when device is asleep */
    	unsigned int fifo_expire_suspended = mdata->fifo_expire[sync][dir] * sleep_latency_multiple;
    	if (display_on && mdata->fifo_expire[sync][dir]) {
-        rq->fifo_time = jiffies + mdata->fifo_expire[sync][dir];
+   		rq_set_fifo_time(rq, jiffies + mdata->fifo_expire[sync][dir]);
    		list_add_tail(&rq->queuelist, &mdata->fifo_list[sync][dir]);
    	} else if (!display_on && fifo_expire_suspended) {
-        rq->fifo_time = jiffies + fifo_expire_suspended;
+   		rq_set_fifo_time(rq, jiffies + fifo_expire_suspended);
    		list_add_tail(&rq->queuelist, &mdata->fifo_list[sync][dir]);
    	}
 }
@@ -109,7 +109,7 @@ maple_expired_request(struct maple_data *mdata, int sync, int data_dir)
 	rq = rq_entry_fifo(list->next);
 
 	/* Request has expired */
-    if (time_after_eq(jiffies, rq->fifo_time))
+	if (time_after_eq(jiffies, rq_fifo_time(rq)))
 		return rq;
 
 	return NULL;
@@ -133,7 +133,7 @@ maple_choose_expired_request(struct maple_data *mdata)
 	 */
 
    if (rq_async_read && rq_sync_read) {
-     if (time_after(rq_sync_read->fifo_time, rq_async_read->fifo_time))
+     if (time_after(rq_fifo_time(rq_sync_read), rq_fifo_time(rq_async_read)))
              return rq_async_read;
    } else if (rq_async_read) {
            return rq_async_read;
@@ -142,7 +142,7 @@ maple_choose_expired_request(struct maple_data *mdata)
    }
 
    if (rq_async_write && rq_sync_write) {
-if (time_after(rq_sync_write->fifo_time, rq_async_write->fifo_time))
+     if (time_after(rq_fifo_time(rq_sync_write), rq_fifo_time(rq_async_write)))
              return rq_async_write;
    } else if (rq_async_write) {
            return rq_async_write;
@@ -161,7 +161,6 @@ maple_choose_request(struct maple_data *mdata, int data_dir)
 
 	/* Increase (non-expired-)batch-counter */
 	mdata->batched++;
-
 
 	/*
 	 * Retrieve request from available fifo list.
@@ -206,7 +205,7 @@ maple_dispatch_requests(struct request_queue *q, int force)
 	struct maple_data *mdata = maple_get_data(q);
 	struct request *rq = NULL;
 	int data_dir = READ;
-	const bool display_on = is_display_on();
+	const bool display_on = state_suspended;
 
 	/*
 	 * Retrieve any expired request after a batch of
@@ -262,22 +261,14 @@ maple_latter_request(struct request_queue *q, struct request *rq)
 	return list_entry(rq->queuelist.next, struct request, queuelist);
 }
 
-static int maple_init_queue(struct request_queue *q, struct elevator_type *e)
+static void *maple_init_queue(struct request_queue *q)
 {
 	struct maple_data *mdata;
-	struct elevator_queue *eq;
-
-	eq = elevator_alloc(q, e);
-	if (!eq)
-		return -ENOMEM;
 
 	/* Allocate structure */
 	mdata = kmalloc_node(sizeof(*mdata), GFP_KERNEL, q->node);
-	if (!mdata) {
-		kobject_put(&eq->kobj);
-		return -ENOMEM;
-	}
-	eq->elevator_data = mdata;
+	if (!mdata)
+		return NULL;
 
 	/* Initialize fifo lists */
 	INIT_LIST_HEAD(&mdata->fifo_list[SYNC][READ]);
@@ -295,10 +286,7 @@ static int maple_init_queue(struct request_queue *q, struct elevator_type *e)
 	mdata->writes_starved = writes_starved;
 	mdata->sleep_latency_multiple = sleep_latency_multiple;
 
-	spin_lock_irq(q->queue_lock);
-	q->elevator = eq;
-	spin_unlock_irq(q->queue_lock);
-	return 0;
+	return mdata;
 }
 
 static void
@@ -406,9 +394,7 @@ static struct elevator_type iosched_maple = {
 static int __init maple_init(void)
 {
 	/* Register elevator */
-	elv_register(&iosched_maple);
-
-	return 0;
+	return elv_register(&iosched_maple);
 }
 
 static void __exit maple_exit(void)
